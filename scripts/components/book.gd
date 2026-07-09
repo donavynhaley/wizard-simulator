e0aa1cb5ec67e78c5c9bfb2fd38fa01ae7c357ce
Donavyn Haley <donavynhaley@gmail.com>
2026-07-09 03:18:10 -0700

    added beginings of books and added particle effect to endless spring

---
+++ scripts/components/book.gd
@@ -0,0 +1,13 @@
+extends Item
+class_name Book
+
+## Books conatin works and images to convey information to the player
+## they can have opened and closed states
+## they can have up to 10 pages worth of information
+## when a book is held in a players hand it is closed.
+## when a book is held in a players hand and they hold left click the book opens and is focused
+## in the focused mode the camera zooms in on the book and its contents can be easily read
+## the player can flip the pages of the book with a and d while reading
+## a book can be place on the crafting table
+## when its place it is open
+## when a player has a book on the table and is in scribe mode they can flip the pages with a & d
